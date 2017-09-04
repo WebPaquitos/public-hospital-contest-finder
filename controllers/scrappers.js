0779f8454c6e5f8f13c385f53161acc2a864d18b
Luís <lf@void.pt>
2017-09-04 11:35:29 +0100

    endpoint to test created

---
+++ controllers/scrappers.js
@@ -0,0 +1,3 @@
+exports.scrape1 = () => {
+    return { message: 'Hello World' };
+};
--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const scrapperController = require('./controllers/scrappers');
 
 app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname + '/client/index.html'));
+});
+
+app.get('/test', function (req, res) {
+    console.log('test');
+    res.json(scrapperController.scrape1());
 });
 
 app.listen(3000, function () {
